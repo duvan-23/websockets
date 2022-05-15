2cb35c0d39a97da8835df027b09f8e0daaff45ca
Duvan Caicedo <duvamendi2@hotmail.com>
2022-05-15 17:24:07 -0500

    modulo productos y mensajes

--- public/main.js
+++ public/main.js
@@ -1,11 +1,39 @@
 const socket = io.connect();
 
-function addMessage(e) {
-    const message = {
-        author: document.getElementById("nombre").value,
-        message: document.getElementById("precio").value,
-    }
+document.querySelector('button').addEventListener('click', () => {
+    addProducto();
+});
+document.getElementById("envioMensaje").addEventListener('click', () => {
+    addMessage();
+});
 
+function addProducto() {
+
+    const producto = {
+        nombre: document.getElementById("nombre").value,
+        precio: document.getElementById("precio").value,
+        foto: document.getElementById("foto").value,
+    }
+    socket.emit("new-product", producto);
+    document.getElementById("nombre").value = "";
+    document.getElementById("precio").value = "";
+    document.getElementById("foto").value = "";
+
+    return false
+}
+function addMessage() {
+    let date = new Date();
+        let day = date.getDate()
+        let month = date.getMonth() + 1
+        let year = date.getFullYear()
+        let hour = date.getHours()
+        let min = date.getMinutes()
+        let sec = date.getSeconds()
+    const message = {
+        author: document.getElementById("email").value,
+        message: document.getElementById("text").value,
+        fecha: `${day}/${month}/${year} ${hour}:${min}:${sec}`,
+    }
     socket.emit("new-message", message);
 
     document.getElementById("text").value = ""
@@ -13,16 +41,40 @@ function addMessage(e) {
 
     return false
 }
-
-function render(data) {
+function renderProductos(data) {
+    let html_titulo=`<h2 style="color:blue;">Productos</h2><br>`;
+    let html1="";
+    let html2="";
+    let html='<h3 class="alert alert-warning">No hay productos</h3>';
+    if(data.length > 0) {
+        html1=`
+    <div class="table-responsive">
+    <table class="table table-dark">
+        <tr style="color: yellow;"> <th>Nombre</th> <th>Precio</th> <th style="text-align:center">Foto</th> </tr>`;
+    html = data.map((elem, index) => {
+        return(`<tr>
+        <td>${elem.nombre}</td>
+        <td>${elem.precio}</td>
+        <td style="text-align:center"><img src="${elem.foto}" alt="" width="30%" height="30%" ></td>
+    </tr>`)
+    }).join(" ")
+    html2=`</table>
+    </div>`;
+    }
+    
+    document.getElementById("products").innerHTML = html_titulo+html1+html+html2;
+}
+function renderMensajes(data) {
+    
     const html = data.map((elem, index) => {
+        
         return(`<div>
-        <strong>${elem.author}</strong>
-        <em>${elem.message}</em>
+        <strong class="text-primary">${elem.author}</strong>  <span style="color:#6F4E37">[${elem.fecha}] :</span>
+        <em class="text-success font-italic">${elem.message}</em>
         </div>`)
     }).join(" ")
 
     document.getElementById("messages").innerHTML = html
 }
-
-socket.on("messages", function(data) {render(data)})
+socket.on("products", function(data) {renderProductos(data)})
+socket.on("messages", function(data) {renderMensajes(data)})
--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express()
 const httpServer = new HttpServer(app)
 const io = new IOServer(httpServer)
 
-const messages = []
+const messages = [];
+const productos = [];
 
 app.use(express.static('./public'))
 app.use(express.urlencoded({extended:true}))
@@ -15,7 +16,7 @@ app.set('views', './public')
 app.set('view engine', 'pug')
 app.get('/', (req, res) => {
     // res.sendFile('index.pug', {root: __dirname})
-    res.render('./views/formulario')
+    res.render('./views/mensajes')
 })
 
 io.on('connection', (socket) => {
@@ -27,6 +28,13 @@ io.on('connection', (socket) => {
         messages.push(data)
 
         io.sockets.emit('messages', messages)
+    })
+    socket.emit('products', productos)
+
+    socket.on('new-product', data => {
+        productos.push(data)
+
+        io.sockets.emit('products', productos)
     })
 })
 
